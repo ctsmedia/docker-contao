d1624e1b56ed1de88cec0deef878359771e0604e
Daniel Schwiperich <daniel.schwiperich@gmail.com>
2018-01-04 23:03:10 +0100

    - add and adjust demo data for contao 4.5 - demo installer can now handle different contao versions - contao 4.5 needs a additional clean cache before contao tasks are run - use mariadb 10.2 to be compatible to innodb requirements by contao ('Die innodb_large_prefix-Option ist nicht aktiv!')

--- script/install-demo.php
+++ script/install-demo.php
@@ -1,5 +1,23 @@
 <?php
 
+echo "Retrieving Contao Version\n";
+$currentContaoVersion = exec("/var/www/share/project/bin/console contao:version");
+echo "Current Contao version: {$currentContaoVersion}\n";
+
+// We have different datasets for some contao versions. Find out which one is needed.
+// There is a Tool for comparing SemVersions https://github.com/composer/semver/
+// But we try to stay single file
+$versionToUseForDemoData = "4.4";
+$comparableContaoVersion = str_replace(".", "", $currentContaoVersion);
+// still not good, if we have a high bugfix / patch number we just cut it. Otherwise 4.5.11 would be higher rated than
+// 6.0.0 . A bugfix release should not affect the demo data version so we just slice it out and make it finally an int
+$comparableContaoVersion = (int)substr($comparableContaoVersion, 0, 3);
+// Select the demo data version to use. We try to stay in line with Contao Versions
+if ($comparableContaoVersion >= 450) {
+    $versionToUseForDemoData = "4.5";
+}
+echo "Demo data version: {$versionToUseForDemoData}\n";
+
 echo "Preparing config\n";
 copy("/var/www/share/data/config/localconfig.php", "/var/www/share/project/system/config/localconfig.php");
 chmod("/var/www/share/project/system/config/localconfig.php", 0777);
@@ -10,7 +28,7 @@ if (!is_dir("/var/www/share/project/app/config")) {
     chmod("/var/www/share/project/app/config/", 0777);
 }
 copy("/var/www/share/data/config/parameters.yml", "/var/www/share/project/app/config/parameters.yml");
-chmod( "/var/www/share/project/app/config/parameters.yml", 0777);
+chmod("/var/www/share/project/app/config/parameters.yml", 0777);
 passthru("chown www-data:www-data /var/www/share/project/app/config/parameters.yml");
 
 echo "Setting up db with demo data\n";
@@ -22,27 +40,24 @@ try {
 
     $pdo->query("USE ${_ENV['PROJECT_NAME']}");
 
-    $demoSql = file_get_contents("/var/www/share/data/demo/contao-demo-4.4.5.sql");
+    $demoSql = file_get_contents("/var/www/share/data/demo/contao-demo-{$versionToUseForDemoData}.sql");
     $pdo->exec($demoSql);
 
 
-
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo "Db Error:\n {$e->getMessage()}\n\n";
 }
-
 
 echo "Installing media files\n";
 passthru("cp -r /var/www/share/data/demo/files/* /var/www/share/project/files/");
 
-echo "Running contao tasks\n";
-passthru("php bin/console contao:filesync");
-passthru("php bin/console contao:symlinks");
-
 echo "Setting permissions\n";
 passthru("chown -R www-data:www-data /var/www/share/project/files/contaodemo");
 
+echo "Running contao tasks and clean cache before\n";
+passthru("rm -fr /var/www/share/project/var/cache/*");
+passthru("php bin/console contao:filesync");
+passthru("php bin/console contao:symlinks");
 
-
-echo "Cleaning cache\n";
+echo "Cleaning cache finally again\n";
 passthru("rm -fr /var/www/share/project/var/cache/*");
